8c59ce321c87cd76b2648d6aa1b0a7124441354b
sherna90 <sherna90@yahoo.es>
2024-03-20 10:45:09 -0300

    plots

--- plot_results.py
+++ plot_results.py
@@ -38,6 +38,15 @@ def plot_entropy(myax, inclass, outclass, label, bins = np.logspace(-8, 0.5, num
     if show_legend:
         myax.legend()
 
+def plot_loss(myax,epochs,loss, label, show_xlabel=False, show_ylabel=False):
+    myax.set_title(str(label), fontsize=12)
+    myax.yaxis.grid(True)
+    myax.plot(epochs,loss)
+    if show_xlabel:
+        myax.set_xlabel('Epochs')
+    if show_ylabel:
+        myax.set_ylabel('Negative log-likelihood')
+
 
 
 file=open('results/gaussian_resnet34.pickle','rb')
@@ -59,4 +68,12 @@ plot_entropy(axes[1], mobilenet_data['train_entropy'][-25:], mobilenet_data['tra
 plot_entropy(axes[2], vgg_data['train_entropy'][-25:], vgg_data['train_entropy'], 'VGG16')
 fig.subplots_adjust(left=0.2)
 plt.savefig('entropy_plots.pdf')
+plt.close()
+
+fig, axes = plt.subplots(nrows=1, ncols=3,sharey=True,)
+plot_loss(axes[0], range(1,101),resnet_data['train_loss'], 'Resnet34', show_ylabel=True)
+plot_loss(axes[1], range(1,101),mobilenet_data['train_loss'], 'MobileNet')
+plot_loss(axes[2], range(1,101),vgg_data['train_loss'], 'VGG16')
+fig.subplots_adjust(left=0.2)
+plt.savefig('nll_plots.pdf')
 plt.close()
